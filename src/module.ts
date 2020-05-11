d6311f3110e60c0c40d850fcaf1b8791279a06a2
David Domkář <dave.domkar@gmail.com>
2020-05-11 19:06:50 +0200

    feat(vuexen): first release

--- src/module.ts
+++ src/module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nuxt/types';
+import path from 'path';
 
-type Options = {
-  a: boolean;
-  b: number;
-  c: string;
+type Options = {};
+
+const vuexenModule: Module<Options> = function(moduleOptions) {
+  // const options: Options = { ...this.options.vuexen, ...moduleOptions };
+
+  this.addPlugin(path.resolve(__dirname, 'plugin.js'));
 };
-
-const vuexenModule: Module<Options> = function (moduleOptions) {};
 
 export default vuexenModule;
---
+++ src/plugin.ts
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+import vuexen from 'vuexen';
+
+const vuexenPlugin = () => {
+  Vue.use(vuexen);
+};
+
+export default vuexenPlugin;
